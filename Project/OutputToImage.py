880d91d10e20357fab8c1ea1c850364406522ef5
Scuck <kermit@conches.org>
2023-02-03 10:12:01 -0800

    Img creation stuff

--- Project/OutputToImage.py
+++ Project/OutputToImage.py
@@ -1,9 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 
-NewIm = Image.new("RGBA", (560,560), color=(0,0,0))
+ImgSiz = 10
+NewIm = Image.new("RGBA", ((ImgSiz*30),(ImgSiz*30)), color=(0,0,0))
 ImgFont = ImageFont.truetype(r'C:\Users\s_KermiWrigh41191\Documents\Code\Git Pages\KermitWright\Project\cutest_things\Cutest Things.ttf', 40)  
 ImgDraw = ImageDraw.Draw(NewIm)
-ImgDraw.text((12,8), "Text that I am \n writing",font=ImgFont, fill=(255,255,255,255))
-ImgDraw.line((200,150,300,200),fill=(255,255,100),width=10)
-ImgDraw.polygon(((400,300),(300,200),(500,50)),fill=(255,0,0),outline=None)
+ImgDraw.polygon(((0,0),(0,26),(15,26)),fill=(255,255,255),outline=None)
+ImgDraw.polygon((((ImgSiz*30),0),((ImgSiz*30),26),((ImgSiz*30-15),26)),fill=(255,255,255))
 NewIm.show()
--- Project/TkinterTest.py
+++ Project/TkinterTest.py
@@ -1,7 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import *
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageDraw
+
+global FClickCo 
+global SClickCo 
+global ClickQ 
+FClickCo = [0,0]
+SClickCo = [0,0]
+ClickQ = True
+
 Win1 = Tk()
 Win1.geometry("800x800")
 Win1Lab = Label(text="V Image Goes Here V",foreground="purple")
@@ -10,15 +18,36 @@ Win1Lab.pack()
 # Create Images
 RawImg = ImageTk.PhotoImage(Image.open(r"Project\Cryptotora.JPG"))
 ButtImg = ImageTk.PhotoImage(Image.new("RGBA", (600,600), color=(0,0,0,0)))
+def CreateImage():
+    ImgSiz = int(ImgCreSiz.get())
+    NewImg = ImageTk.PhotoImage(Image.new("RGBA", ((ImgSiz*30),(ImgSiz*30)), color=(0,0,0,0)))
+    # Triangles (on Creation) have a side length of 30 and a height of 26.
+    ImgCre = ImageDraw.Draw(NewImg)
+    ImgCre.polygon((0,0),(0,26),(15,26),fill=(255,255,255),outline=None)
+    ImgCre.polygon((((ImgSiz*30),0),((ImgSiz*30),26),((ImgSiz*30-15),26)),fill=(255,255,255))
 
-# Button Function
-def ButtonPress():
-    messagebox.showinfo("Button Box", "Button Pressed")
 
+# CLICKABLE FISH!!!!!
 def CursorFind(event):
-    Cursorx= event.x 
-    Cursory= event.y
-    print("Clicked!", f"{Cursorx},{Cursory}")
+    global FClickCo 
+    global SClickCo 
+    global ClickQ 
+    if ClickQ == True:
+        FClickCo[0] = event.x
+        FClickCo[1] = event.y
+        ClickQ = False
+        print("First Click At:", f"{FClickCo}")
+    else:
+        SClickCo[0] = event.x 
+        SClickCo[1] = event.y
+        ClickQ = True
+        print("Second Click At:", f"{SClickCo}")
+
+# Image Creation Specs
+ImgCreSiz = Entry(Win1)
+ImgCreSiz.pack()
+ImgCreFin = Button(Win1, text='Generate Image', command=CreateImage)
+ImgCreFin.pack()
 
 # Create Image Frame
 ImgBox = Frame(Win1,width=600,height=600)
@@ -28,13 +57,6 @@ ImgBox.place(anchor='center', relx=0.5, rely=0.5)
 ImgCont = Label(ImgBox, image = RawImg)
 ImgCont.bind("<Button-1>", CursorFind)
 ImgCont.pack()
-
-# Create Click box
-# ClickBox = Canvas(Win1, width=600, height=600)
-# ClickBox.bind("<Button-1>", CursorFind)
-# ClickBox.pack()
-# ClickBox.place(anchor='center', relx=0.5, rely=0.5)
-
 
 # Make it Work
 Win1.mainloop()
